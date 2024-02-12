7db4292f1416875c3eaeae11e43e1ed837884d0a
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2024-02-13 00:00:19 +0200

    add redirect to settings manually alert dialog if cant ask for permission anymore

--- Mapnector/app/src/main/java/com/vippygames/mapnector/NotificationPermissions.java
+++ Mapnector/app/src/main/java/com/vippygames/mapnector/NotificationPermissions.java
@@ -3,8 +3,12 @@ package com.vippygames.mapnector;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -17,6 +21,24 @@ public class NotificationPermissions {
     public void requestPostNotificationsPermission(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+        } else {
+            manualRequestNotifications(activity);
         }
+    }
+
+    private void manualRequestNotifications(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Enable Notifications");
+        builder.setMessage("We will redirect you to app settings. Please enter the notifications section and enable them.");
+        builder.setPositiveButton("Open Settings", (dialog, which) -> {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
+            intent.setData(uri);
+            activity.startActivity(intent);
+        });
+        builder.setNegativeButton("Cancel", null);
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
